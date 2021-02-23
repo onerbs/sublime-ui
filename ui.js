acd87d95f2e47f3ecda4e5bf096190277fc8021c
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-02-23 15:00:47 -0600

    fmt: Use camel case for function names

--- ui.js
+++ ui.js
@@ -14,11 +14,11 @@ const outputExt = '.sublime-color-scheme'
 
 const fsOptions = { encoding: 'utf-8' }
 
-function get_key_value(item) {
+function getKeyValue(item) {
   return item.slice(1).split(/\s*=\s*/)
 }
 
-function get_file_content(file) {
+function getFileContent(file) {
   const filePath = path.join(sourceDir, file)
   const raw = fs.readFileSync(filePath, fsOptions)
   const content = {
@@ -27,29 +27,28 @@ function get_file_content(file) {
   }
   for (line of raw.split(/\r?\n/)) {
     if (line.startsWith('$')) {
-      const [key, value] = get_key_value(line)
+      const [key, value] = getKeyValue(line)
       content.variables[key] = value
     }
     if (line.startsWith('%')) {
-      const [key, value] = get_key_value(line)
+      const [key, value] = getKeyValue(line)
       content.globals[key] = value
     }
   }
   return content
 }
 
-function output_name(name) {
+function getOutputName(name) {
   return name.replace(sourceExt, outputExt)
 }
 
-function get_source_files() {
+function getSourceFiles() {
   const files = fs.readdirSync(sourceDir)
   return files.filter(file => file.endsWith(sourceExt))
 }
 
-get_source_files().forEach(it => {
-  const content = { ...get_file_content(it), ...rules }
-  const result = JSON.stringify(content)
-  const filePath = path.join(outputDir, output_name(it))
-  fs.writeFileSync(filePath, result, fsOptions)
+getSourceFiles().forEach(it => {
+  const data = JSON.stringify({ ...getFileContent(it), ...rules })
+  const filePath = path.join(outputDir, getOutputName(it))
+  fs.writeFileSync(filePath, data, fsOptions)
 })
