7fea3cfe3842e021e0aaee54fedb76c3a15fa669
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-06-07 16:10:38 -0500

    refactor  - untrack changes in `rules.mjs`

--- cli.mjs
+++ cli.mjs
@@ -1,10 +1,19 @@
 import { compile } from './lib/ui.mjs'
-import { rules } from './rules.mjs'
+import { existsSync } from 'fs'
 
-compile({
-  outDir: 'Schemes',
-  outExt: '.sublime-color-scheme',
-  srcDir: 'Palettes',
-  srcExt: '.palette',
-  userRules: rules,
-})
+function main(rules = []) {
+  compile({
+    outDir: 'Schemes',
+    outExt: '.sublime-color-scheme',
+    srcDir: 'Palettes',
+    srcExt: '.palette',
+    userRules: rules,
+  })
+}
+
+if (existsSync('./rules.mjs'))
+  import('./rules.mjs')
+    .then(mod => main(mod.rules))
+
+else
+  main()
